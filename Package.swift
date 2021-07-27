a360a0a6138a9355c5ebfdf871e37fd8c7147a7f
Ryan Kelly <ryan@rfk.id.au>
2021-07-27 11:54:58 +1000

    Update to include more packages, and use appservices as a submodule.

--- Package.swift
+++ Package.swift
@@ -9,35 +9,58 @@ let package = Package(
         .library(name: "Viaduct", targets: ["Viaduct"]),
         .library(name: "Nimbus", targets: ["Nimbus"]),
         .library(name: "CrashTest", targets: ["CrashTest"]),
+        .library(name: "Logins", targets: ["Logins"]),
+        .library(name: "FxAClient", targets: ["FxAClient"]),
     ],
     dependencies: [
-        .package(name: "Glean", url: "https://github.com/mozilla/glean-swift", from: "38.0.0")
+        // TODO: ship Glean via this same bundle?
+        .package(name: "Glean", url: "https://github.com/mozilla/glean-swift", from: "39.0.4"),
+        .package(name: "SwiftKeychainWrapper", url: "https://github.com/jrendel/SwiftKeychainWrapper", from: "4.0.1")
     ],
     targets: [
         .target(
+            // TODO: we should make a proper package for this.
+            name: "Sync15",
+            path: "external/application-services/megazords/ios",
+            sources: ["SyncUnlockInfo.swift"]
+        ),
+        .target(
             name: "RustLog",
             dependencies: ["MozillaRustComponents"],
-            path: "components/rustlog"
+            path: "external/application-services/megazords/ios/RustLog"
         ),
         .target(
             name: "Viaduct",
             dependencies: ["MozillaRustComponents"],
-            path: "components/viaduct"
+            path: "external/application-services/megazords/ios/Viaduct"
         ),
         .target(
             name: "Nimbus",
             dependencies: ["MozillaRustComponents", "Glean"],
-            path: "components/nimbus"
+            path: "external/application-services/components/nimbus/ios"
         ),
         .target(
             name: "CrashTest",
             dependencies: ["MozillaRustComponents"],
-            path: "components/crashtest"
+            path: "external/application-services/components/crashtest/ios"
+        ),
+        .target(
+            name: "Logins",
+            dependencies: ["MozillaRustComponents", "Sync15"],
+            path: "external/application-services/components/logins/ios"
+        ),
+        .target(
+            name: "FxAClient",
+            dependencies: ["MozillaRustComponents", "SwiftKeychainWrapper"],
+            path: "external/application-services/components/fxa-client/ios"
         ),
         .binaryTarget(
             name: "MozillaRustComponents",
-            url: "https://user.fm/files/v2-707bfbe4deabae4d16bf9c200256cd95/MozillaRustComponents.xcframework.zip",
-            checksum: "8a3451f5cc65225e0bbebb8058d51c27a5d7000d57c9e587a9887e9b63cfa75e"
+            // For local testing, you can point at an (unzipped) XCFramework that's part of the repo.
+            //path: "./MozillaRustComponents.xcframework"
+            // For release artifacts, reference the MozillaRustComponents as a URL with checksum.
+            url: "https://github.com/rfk/rust-components-swift/releases/download/0.2.0/MozillaRustComponents.xcframework.zip",
+            checksum: "8efc6ebb4123763a2721e116440fdf40a6f44a1887d187c0c0507d41aad04570"
         )
     ]
 )
---
+++ make_tag.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#
+
+git commit -a --amend
+git tag -f 0.2.0
